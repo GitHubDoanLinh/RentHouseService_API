e32d23256e582752ae932e3fd6e50eab119aa0a8
Doan Huu Linh <doanlinh1510@gmail.com>
2024-10-15 20:21:17 +0700

    first commit -base

---
+++ src/main/java/com/example/renthouseweb_be/RentHouseWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.renthouseweb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentHouseWebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentHouseWebBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/renthouseweb_be/RentHouseWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.renthouseweb_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentHouseWebBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
