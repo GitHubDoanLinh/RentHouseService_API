87b705642e2857ad70dc843e86db54b63139249b
Doan Huu Linh <doanlinh1510@gmail.com>
2024-12-17 00:46:16 +0700

    update endpoint for update house

--- src/main/java/com/example/renthouseweb_be/controller/HouseController.java
+++ src/main/java/com/example/renthouseweb_be/controller/HouseController.java
@@ -53,18 +53,29 @@ public class HouseController {
 
     }
 
+    @PutMapping("/update/{id}")
+    public ResponseEntity<CreateHouseResponse> update(@PathVariable Long id, @RequestBody CreateHouseRequest request) {
+        try {
+            request.setId(id);
+            houseService.save(request);
+            return new ResponseEntity<>(new CreateHouseResponse(true, "MS-HO-01"), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(new CreateHouseResponse(false, "ER-HO-01"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<House> delete(@PathVariable Long id) {
         houseService.delete(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<House> update(@PathVariable Long id, @RequestBody House house) {
-        house.setId(id);
-        houseService.save(house);
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
+//    @PutMapping("/update/{id}")
+//    public ResponseEntity<House> update(@PathVariable Long id, @RequestBody House house) {
+//        house.setId(id);
+//        houseService.save(house);
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
 
     @GetMapping("/{id}")
     public ResponseEntity<HouseDTO> findById(@PathVariable Long id) {
--- src/main/java/com/example/renthouseweb_be/requests/CreateHouseRequest.java
+++ src/main/java/com/example/renthouseweb_be/requests/CreateHouseRequest.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Data
 public class CreateHouseRequest {
+    private Long id;
     private String name;
     private String description;
     private double price;
